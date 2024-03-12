f06a211f1dc54978437ae196a9592265b7cb7d70
shalakaharale <haralesn@gmail.com>
2024-03-12 22:39:03 +0530

    Tooltip app

---
+++ src/Tooltip.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Tooltip.css";
+
+const Tooltip = ({ position, children }) => {
+  const [showTooltip, setShowTooltip] = useState(true);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+  console.log("position", position);
+  return (
+    <div
+      className="tooltip-container"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {children}
+      {showTooltip && (
+        <div className={`${position}`}>Thanks for hovering! I am a tooltip</div>
+      )}
+    </div>
+  );
+};
+
+export default Tooltip;
